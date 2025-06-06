0c55ff71ee06939d5b9352852e285c5a8f8697de
Liam Nutley <evildeadfan123@gmail.com>
2025-06-06 02:53:21 +0100

    Update face-identifier.py
    Updated notes and tidied variable layouts in functions.

--- face-identifier.py
+++ face-identifier.py
@@ -1,48 +1,51 @@
 # Name: Liam Nutley
 # Email: liam.nutley@yahoo.ie
-# Face Identifier
+# Face Identifier using OpenCV (cv2)
 
-# Importing all required packages
+# Import all required packages
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-import PIL as pil
+import PIL as pil	# Python Imaging Library (PIL) adds support for opening, manipulating, and saving image file formats.
 
 
 # Read in the cascade classifiers for face and eyes
-face_cascade = cv2.CascadeClassifier(r'C:\Users\liamn\Desktop\data-engineer-takehome\haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier(r'.\haarcascade_frontalface_default.xml')
 
-# create a function to detect face
+# Create a function to detect face
 def adjusted_detect_face(img):
-	
 	face_img = img.copy()
-	
 	face_rect = face_cascade.detectMultiScale(face_img,
-	scaleFactor = 1.2,
-	minNeighbors = 5)
+						  scaleFactor = 1.2,
+						  minNeighbors = 5)
 	
 	for (x, y, w, h) in face_rect:
-		cv2.rectangle(face_img, (x, y),
-					(x + w, y + h), (255, 255, 255), 10)\
-		
-	return face_img
+		cv2.rectangle(face_img, 
+			      (x, y),	
+			      (x + w, y + h), 
+			      (255, 255, 255), 
+			       10)
+		return face_img
 
 
-# create a function to detect eyes
+# Create a function to detect eyes
 def detect_eyes(img):
 	
 	eye_img = img.copy()
 	eye_rect = eye_cascade.detectMultiScale(eye_img,
-	scaleFactor = 1.2,
-	minNeighbors = 5)
+						scaleFactor = 1.2,
+						minNeighbors = 5)
 	
 	for (x, y, w, h) in eye_rect:
-		cv2.rectangle(eye_img, (x, y),
-					(x + w, y + h), (255, 255, 255), 10)	
+		cv2.rectangle(eye_img, 
+			      (x, y),
+			      (x + w, y + h),
+			      (255, 255, 255), 
+			       10)	
 	return eye_img
 
 # Reading in the image and creating copies
-img = cv2.imread(r'C:\Users\liamn\Desktop\data-engineer-takehome\picture.jpg')
+img = cv2.imread(r'.\picture.jpg') # Image to be read into cv2
 img_copy1 = img.copy()
 img_copy2 = img.copy()
 img_copy3 = img.copy()
